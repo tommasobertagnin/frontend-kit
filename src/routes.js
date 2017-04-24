5e9287b330dd700ab6e586fdda5b7e016cec8985
tommasobertagnin <tommasobertagnin@gmail.com>
2017-04-24 20:22:08 +0200

    add 404 page fallback

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import { Route, NavLink } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import PageA from './components/PageA'
 import PageB from './components/PageB'
 import Menu from './components/Menu'
 
 export default <div>
   <Menu />
-  <Route exact path="/" component={PageA} />
-  <Route path="/page-b" component={PageB} />
+  <Switch>
+    <Route exact path="/" component={PageA} />
+    <Route path="/page-b" component={PageB} />
+    <Route render={() => (
+      // 404 page...
+      <div className="page">
+        <p className="header L">
+          Sorry... What were you looking for? :-)
+        </p>
+      </div>
+    )} />
+  </Switch>
 </div>
