c2f7a08be6eed7e054eb90797047cc90147ac6d4
tommasobertagnin <tommasobertagnin@gmail.com>
2017-04-25 10:57:38 +0200

    add required PropTypes

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -7,5 +8,10 @@ const Root = ({ store, routes }) => (
     <BrowserRouter>{routes}</BrowserRouter>
   </Provider>
 )
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+  routes: PropTypes.object.isRequired,
+}
 
 export default Root
