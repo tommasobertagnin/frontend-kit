2545608ad63278bfd04592ddfa0b7767ae907a41
tommasobertagnin <tommasobertagnin@gmail.com>
2017-04-25 10:54:06 +0200

    correct production VS development require

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./createStoreDev')
+  module.exports = require('./createStoreProd')
 }
 else {
-  module.exports = require('./createStoreProd')
+  module.exports = require('./createStoreDev')
 }
